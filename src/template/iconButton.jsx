3ffab3f9bef0be72e54269dcbf8338bf58d8447e
Vinicius Marques <vncsmrqs@gmail.com>
2018-11-23 06:15:12 -0300

    Grid, Buttons, Routes and Forms

---
+++ src/template/iconButton.jsx
@@ -0,0 +1,15 @@
+import React from'react'
+
+export default props => {
+    if(props.hide) {
+        return null
+    } else {
+        return (
+        <button className={'btn btn-' + props.style}
+            onClick={props.onCLick}>
+            <i className={'fa fa-'+props.icon}></i>
+        </button>
+        )
+    }
+    
+}
